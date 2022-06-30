95b7fc30e3e7f43d3d3a1fd71e6ddb2fcac9b98d
Vitor Rodrigues Ferreira <vitor.ferreira@rocketwave.tech>
2022-06-30 01:08:43 -0300

    add structure to handle process given bound-type

---
+++ src/app/core/process/bound-type.ts
@@ -0,0 +1,15 @@
+export default class BoundType {
+  static readonly CPU  = new BoundType('CPU', 0);
+  static readonly IO_DISC = new BoundType('IO_DISC', 1.2);
+  static readonly IO_TAPE = new BoundType('IO_TAPE', 1.4);
+  static readonly IO_TERMINAL = new BoundType('IO_TERMINAL', 1);
+
+  private constructor(
+    private readonly key: string,
+    public readonly delayFactor: number
+  ) { }
+
+  toString() {
+    return this.key;
+  }
+}
